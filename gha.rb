a9d2ff43b85373e54dda4abb4ede96d7de55eefc
James Martin <jamesmartin@github.com>
2018-01-21 10:38:07 -0800

    Homepage routes layout

--- gha.rb
+++ gha.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 
 get "/" do
-  "Testing..."
+  erb <<~EOS
+    <h2>Interesting routes:</h2>
+    <pre>
+      <li>/             => This homepage</li>
+      <li>/user_authz   => User authorization callback URL</li>
+      <li>/setup        => (Optional) setup URL</li>
+      <li>/hook         => Receives incoming installation and modification Webhooks</li>
+    </pre>
+  EOS
 end

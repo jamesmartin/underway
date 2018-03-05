5175159a08ead19da7817943009f3a1b48c90e9d
James Martin <jamesmartin@github.com>
2018-03-05 21:42:42 +1100

    Move info routes out of example app

--- example/app.rb
+++ example/app.rb
@@ -13,18 +13,7 @@ include Sinatra::Underway::AppInfo
 
 get "/" do
   erb <<~EOS
-    <h2>Interesting routes:</h2>
-    <pre>
-      <li>/             => This homepage</li>
-      <li>/user_authz   => User authorization callback URL</li>
-      <li>/setup        => (Optional) setup URL</li>
-      <li>/hook         => Receives incoming installation and modification Webhooks</li>
-      <li>/jwt          => Generates a JWT for authentication as the App</li>
-    </pre>
-    <h2>Private PEM file</h2>
-    <pre>
-      #{Underway::Settings.config.private_key_filename}
-    </pre>
+    <a href="/info">Underway App Information</a>
   EOS
 end
 
@@ -34,14 +23,8 @@ end
 
 get "/setup" do
   debug_route(request)
-  erb "Setting up installation: #{params["installation_id"]}"
 end
 
 post "/hook" do
   debug_route(request)
-end
-
-get "/jwt" do
-  content_type :json
-  Underway::Api.generate_jwt
 end
--- lib/underway/sinatra/app_info.rb
+++ lib/underway/sinatra/app_info.rb
@@ -15,6 +15,31 @@ module Sinatra
   module Underway
     module AppInfo
       def self.registered(app)
+        app.get "/info" do
+          erb <<~EOS
+            <h1>Underway</h1>
+            <h2>Interesting routes:</h2>
+            <pre>
+              <li>/info                                                                                           => This page</li>
+              <li><a href="/info/app">/info/app</a>                                                               => Information about the configured GitHub App</li>
+              <li><a href="/info/app/jwt">/info/app/jwt</a>                                                       => Generates a JWT for authentication as this App</li>
+              <li><a href="/info/app/installations">/info/app/installations</a>                                   => A list of installations associated with this App</li>
+              <li><a href="/info/app/installations/1">/info/app/installations/:id</a>                             => Information about the given installation of this App</li>
+              <li><a href="/info/app/installations/1/access_token">/info/app/installations/:id/access_token</a>   => A valid access token for accessing the given installation as this App</li>
+              <li><a href="/info/app/installations/1/repositories">/info/app/installations/:id/repositories</a>   => A list of all repositories accessible to the installation of this App</li>
+            </pre>
+            <h2>Private PEM file</h2>
+            <pre>
+              #{::Underway::Settings.config.private_key_filename}
+            </pre>
+          EOS
+        end
+
+        app.get "/info/app/jwt" do
+          content_type :json
+          ::Underway::Api.generate_jwt
+        end
+
         app.get "/info/app" do
           content_type :json
           ::Underway::SawyerToJson.convert(gh_api("/app"))

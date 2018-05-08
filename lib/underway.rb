9055b75e19c7eca76507255cf9e7d85c2866f693
James Martin <jamesmartin@github.com>
2018-05-09 07:59:26 +1000

    Make underway/sinatra optional so we don't have to depend on Sinatra

--- lib/underway.rb
+++ lib/underway.rb
@@ -3,7 +3,6 @@ require "underway/database"
 require "underway/logger"
 require "underway/sawyer_to_json"
 require "underway/settings"
-require "underway/sinatra"
 require "underway/token_cache"
 require "underway/version"
 
